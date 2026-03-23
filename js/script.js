07579177fb27a5d38f771296ea1360a35ea54879
Marcos Nathanael <marcos.nathanael_@hotmail.com>
2026-03-23 13:41:20 -0300

    Refactor menu toggle functionality and update styles
    - Changed file permissions for multiple files to executable.
    - Updated CSS styles in header.css and style.css for better menu interaction.
    - Replaced menu toggle icon images with SVGs for improved scalability.
    - Modified JavaScript to handle SVG icon visibility based on menu state.
    - Enhanced accessibility by updating alt text for menu icons.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
 // Obtendo referências para os elementos HTML que serão usados
 const btnMobile = document.getElementById("button-mobile");
 const nav = document.getElementById("nav");
-const menuIcon = document.getElementById("menu-icon");
+const menuIconOpen = document.getElementById("menu-icon-open");
+const menuIconClose = document.getElementById("menu-icon-close");
 const linksMenu = document.querySelectorAll(".header-menu-link");
 
-// Função para alternar o menu (abrir/fechar) e alterar o ícone correspondente
+// Função para alternar o menu (abrir/fechar)
 function toggleMenu() {
   nav.classList.toggle("active");
 
-  // Verifica se o menu está aberto ou fechado pela presença da classe 'active'
+  // Trocar entre os ícones
   if (nav.classList.contains("active")) {
-    // Se o menu está aberto, muda dinamicamente o ícone para representar o menu aberto
-    menuIcon.src = "img/menu-closer.svg"; // Altera a imagem do ícone
-    menuIcon.alt = "Menu Aberto"; // Atualiza o texto alternativo do ícone
+    menuIconOpen.style.display = "none";
+    menuIconClose.style.display = "block";
   } else {
-    // Se o menu está fechado, muda dinamicamente o ícone para representar o menu fechado
-    menuIcon.src = "img/menu-togle.svg";
-    menuIcon.alt = "Menu Fechado";
+    menuIconOpen.style.display = "block";
+    menuIconClose.style.display = "none";
   }
 }
 // Adiciona um ouvinte de evento para detectar cliques no botão e chama a função toggleMenu()
 // Função para fechar o menu quando um link é clicado
 function closeMenuOnClick() {
   nav.classList.remove("active"); // Fecha o menu removendo a classe 'active'
-  menuIcon.src = "assets/img/menu-togle.svg"; // Atualiza o ícone para representar o menu fechado
-  menuIcon.alt = "Menu Fechado";
+  menuIconOpen.style.display = "block";
+  menuIconClose.style.display = "none";
 }
 
 btnMobile.addEventListener("click", toggleMenu); // Adiciona o evento para abrir/fechar o menu ao clicar no botão
